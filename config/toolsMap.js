b4b6b407b2eb1f8af78f131daa2e4dfb584ca964
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-03 23:39:06 -0300

    Update toolsMap.js

--- config/toolsMap.js
+++ config/toolsMap.js
@@ -1,1 +1,36 @@
+// config/toolsMap.js
 
+import { generateHTML } from "../tools/htmlGenerator.js";
+import { fetchApi } from "../tools/fetchApi.js";
+
+export const toolsMap = {
+  
+  generate_html: {
+    name: "generate_html",
+    description: "Gera um site HTML baseado em um prompt",
+    execute: async (input) => {
+      return generateHTML(input);
+    }
+  },
+
+  fetch_crypto: {
+    name: "fetch_crypto",
+    description: "Busca preço de criptomoedas",
+    execute: async () => {
+      return await fetchApi(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+    }
+  },
+
+  fetch_weather: {
+    name: "fetch_weather",
+    description: "Busca clima de uma cidade",
+    execute: async (city) => {
+      return await fetchApi(
+        `https://api.open-meteo.com/v1/forecast?latitude=-16.3&longitude=-48.9&current_weather=true`
+      );
+    }
+  }
+
+};
