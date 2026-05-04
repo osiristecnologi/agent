6e57246ae2f602ef34961be5e917b28020ee2c53
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-03 23:12:55 -0300

    Update apiAgent.js

--- agents/apiAgent.js
+++ agents/apiAgent.js
@@ -1,1 +1,8 @@
+import { fetchApi } from "../tools/fetchApi.js";
 
+export const apiAgent = {
+  execute: async (input) => {
+    const data = await fetchApi("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+    return JSON.stringify(data, null, 2);
+  }
+};
