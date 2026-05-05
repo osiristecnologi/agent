5ee074f2a215a73016f3380000ef26c58f588ed0
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-04 22:51:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import { brain } from "./brain/brain.js";
 import { loadConfig } from "./brain/configStore.js";
 import { clearSession, getSession } from "./memory/session.js";
-
+// import { loadConfig } from "./brain/configStore.js";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
